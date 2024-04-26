b5ea99df78bba07c5774dca7a7fc77f66e214369
Simon-Honore <simon.honore6@gmail.com>
2024-04-26 11:25:39 +0200

    creating the structure

---
+++ views/category/show.php
@@ -0,0 +1,5 @@
+<?php require VIEW_PATH . '/layout/header.php' ?>
+
+<h1>Ma category</h1>
+
+<?php require VIEW_PATH . '/layout/footer.php' ?>
---
+++ views/post/index.php
@@ -0,0 +1,5 @@
+<?php require VIEW_PATH . '/layout/header.php' ?>
+
+<h1>Mon Blog</h1>
+
+<?php require VIEW_PATH . '/layout/footer.php' ?>
